826d2ea33f74f0193fafa96419564334cd9ca2d1
bulentozkir <bulentozkir@hotmail.com>
2023-11-24 22:56:23 +0300

    Create removeMMAfromVMs.ps1

---
+++ script/removeMMAfromVMs.ps1
@@ -0,0 +1,16 @@
+# Get the list of Azure Arc machines in the subscriptions
+$machines = Get-AzConnectedMachine | Select-Object Name, ResourceGroupName
+
+# Loop through each machine and delete the extension if it exists
+foreach ($machine in $machines) {
+  # Check if the extension is installed
+  $extension = Get-AzConnectedMachineExtension -MachineName $machine.Name -Name AzureMonitorWindowsAgent -ResourceGroupName $machine.ResourceGroupName
+  if ($extension) {
+    # Delete the extension
+    Remove-AzConnectedMachineExtension -MachineName $machine.Name -Name AzureMonitorWindowsAgent -ResourceGroupName $machine.ResourceGroupName -Force
+    Write-Host "Deleted AzureMonitorWindowsAgent extension from $($machine.Name)"
+  }
+  else {
+    Write-Host "AzureMonitorWindowsAgent extension not found on $($machine.Name)"
+  }
+}
