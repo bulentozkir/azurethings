6c7c456d7fe54a02b5e387c50a5dd997984402ca
bulentozkir <bulentozkir@hotmail.com>
2023-12-07 13:58:28 +0300

    Update removeMMAfromArcVMs.ps1

--- script/removeMMAfromArcVMs.ps1
+++ script/removeMMAfromArcVMs.ps1
@@ -4,13 +4,13 @@ $machines = Get-AzConnectedMachine | Select-Object Name, ResourceGroupName
 # Loop through each machine and delete the extension if it exists
 foreach ($machine in $machines) {
   # Check if the extension is installed
-  $extension = Get-AzConnectedMachineExtension -MachineName $machine.Name -Name AzureMonitorWindowsAgent -ResourceGroupName $machine.ResourceGroupName
+  $extension = Get-AzConnectedMachineExtension -MachineName $machine.Name -Name MicrosoftMonitoringAgent -ResourceGroupName $machine.ResourceGroupName
   if ($extension) {
     # Delete the extension
-    Remove-AzConnectedMachineExtension -MachineName $machine.Name -Name AzureMonitorWindowsAgent -ResourceGroupName $machine.ResourceGroupName -Force
+    Remove-AzConnectedMachineExtension -MachineName $machine.Name -Name MicrosoftMonitoringAgent -ResourceGroupName $machine.ResourceGroupName -Force
     Write-Host "Deleted AzureMonitorWindowsAgent extension from $($machine.Name)"
   }
   else {
-    Write-Host "AzureMonitorWindowsAgent extension not found on $($machine.Name)"
+    Write-Host "MicrosoftMonitoringAgent extension not found on $($machine.Name)"
   }
 }
