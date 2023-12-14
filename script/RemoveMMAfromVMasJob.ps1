dfb0203b15c61c95fc48055b886727c9c2769517
bulentozkir <bulentozkir@hotmail.com>
2023-12-14 16:56:06 +0300

    Update RemoveMMAfromVMasJob.ps1

--- script/RemoveMMAfromVMasJob.ps1
+++ script/RemoveMMAfromVMasJob.ps1
@@ -1,7 +1,17 @@
-$myVM = Get-AzConnectedMachine
-foreach($vm in $myVM)
-{
+# Get the list of Azure VM in the subscriptions
+$machines = Get-AzVM | Select-Object Name, ResourceGroupName
+ 
+# Loop through each machine and delete the extension if it exists
+foreach ($machine in $machines) {
 start-job -name myjob -ScriptBlock {
-	Write-Host "Deleted MicrosoftMonitoringAgent extension from $($vm.Name)"
-	Remove-AzConnectedMachineExtension -MachineName $vm.Name -Name MicrosoftMonitoringAgent -ResourceGroupName $vm.ResourceGroupName -Verbose
+  # Check if the extension is installed
+  $extension = Get-AzVMExtension -VMName $machine.Name -Name MicrosoftMonitoringAgent -ResourceGroupName $machine.ResourceGroupName
+  if ($extension) {
+    # Delete the extension
+    Remove-AzVMExtension -ResourceGroupName $machine.ResourceGroupName  -Name MicrosoftMonitoringAgent -VMName $machine.Name -Force
+    Write-Host "Deleted MicrosoftMonitoringAgent extension from $($machine.Name)"
+  }
+  else {
+    Write-Host "MicrosoftMonitoringAgent extension not found on $($machine.Name)"
+  }
 } ; Wait-Job myjob;}
