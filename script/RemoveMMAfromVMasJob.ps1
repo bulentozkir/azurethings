1a03685fb273d019ee35e7626ad9c855a4b7d6da
bulentozkir <bulentozkir@hotmail.com>
2023-12-22 14:24:48 +0300

    Update RemoveMMAfromVMasJob.ps1

--- script/RemoveMMAfromVMasJob.ps1
+++ script/RemoveMMAfromVMasJob.ps1
@@ -15,3 +15,33 @@ start-job -name myjob -ScriptBlock {
     Write-Host "MicrosoftMonitoringAgent extension not found on $($machine.Name)"
   }
 } ; Wait-Job myjob;}
+
+
+foreach ($machine in $machines) {
+start-job -name myjob -ScriptBlock {
+  # Check if the extension is installed
+  $extension = Get-AzVMExtension -VMName $machine.Name -Name OmsAgentForLinux -ResourceGroupName $machine.ResourceGroupName
+  if ($extension) {
+    # Delete the extension
+    Remove-AzVMExtension -ResourceGroupName $machine.ResourceGroupName  -Name OmsAgentForLinux -VMName $machine.Name -Force
+    Write-Host "Deleted OmsAgentForLinux extension from $($machine.Name)"
+  }
+  else {
+    Write-Host "OmsAgentForLinux extension not found on $($machine.Name)"
+  }
+} ; Wait-Job myjob;}
+
+
+foreach ($machine in $machines) {
+start-job -name myjob -ScriptBlock {
+  # Check if the extension is installed
+  $extension = Get-AzVMExtension -VMName $machine.Name -Name DependencyAgent -ResourceGroupName $machine.ResourceGroupName
+  if ($extension) {
+    # Delete the extension
+    Remove-AzVMExtension -ResourceGroupName $machine.ResourceGroupName  -Name DependencyAgent -VMName $machine.Name -Force
+    Write-Host "Deleted DependencyAgent extension from $($machine.Name)"
+  }
+  else {
+    Write-Host "DependencyAgent extension not found on $($machine.Name)"
+  }
+} ; Wait-Job myjob;}
