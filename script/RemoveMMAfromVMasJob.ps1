9f5e88615b24fb2ab1dc3c7a33f8c689034a6a06
bulentozkir <bulentozkir@hotmail.com>
2023-12-22 14:48:28 +0300

    Update RemoveMMAfromVMasJob.ps1

--- script/RemoveMMAfromVMasJob.ps1
+++ script/RemoveMMAfromVMasJob.ps1
@@ -10,6 +10,8 @@ start-job -name myjob -ScriptBlock {
     # Delete the extension
     Remove-AzVMExtension -ResourceGroupName $machine.ResourceGroupName  -Name MicrosoftMonitoringAgent -VMName $machine.Name -Force
     Write-Host "Deleted MicrosoftMonitoringAgent extension from $($machine.Name)"
+    Remove-AzVMExtension -ResourceGroupName $machine.ResourceGroupName  -Name DependencyAgent -VMName $machine.Name -Force
+    Write-Host "Deleted DependencyAgent extension from $($machine.Name)"
   }
   else {
     Write-Host "MicrosoftMonitoringAgent extension not found on $($machine.Name)"
@@ -25,23 +27,10 @@ start-job -name myjob -ScriptBlock {
     # Delete the extension
     Remove-AzVMExtension -ResourceGroupName $machine.ResourceGroupName  -Name OmsAgentForLinux -VMName $machine.Name -Force
     Write-Host "Deleted OmsAgentForLinux extension from $($machine.Name)"
-  }
-  else {
-    Write-Host "OmsAgentForLinux extension not found on $($machine.Name)"
-  }
-} ; Wait-Job myjob;}
-
-
-foreach ($machine in $machines) {
-start-job -name myjob -ScriptBlock {
-  # Check if the extension is installed
-  $extension = Get-AzVMExtension -VMName $machine.Name -Name DependencyAgent -ResourceGroupName $machine.ResourceGroupName
-  if ($extension) {
-    # Delete the extension
     Remove-AzVMExtension -ResourceGroupName $machine.ResourceGroupName  -Name DependencyAgent -VMName $machine.Name -Force
     Write-Host "Deleted DependencyAgent extension from $($machine.Name)"
   }
   else {
-    Write-Host "DependencyAgent extension not found on $($machine.Name)"
+    Write-Host "OmsAgentForLinux extension not found on $($machine.Name)"
   }
 } ; Wait-Job myjob;}
